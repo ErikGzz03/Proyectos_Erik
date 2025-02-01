d99cbff2e4489c9e64ef79bd7f4914c291026f74
ErikGzz03 <168046613+ErikGzz03@users.noreply.github.com>
2025-02-01 12:30:20 -0600

    Add files via upload

---
+++ Sumergete en la programacion con JAVA/Desafio_3raClase.js
@@ -0,0 +1,37 @@
+// Contador para llegar a 10 
+
+/*
+let contador = 1; 
+while (contador < 11) {
+    console.log(contador);
+    contador = contador + 1;
+}
+*/
+
+// Contador del 10 al 0 
+/*
+let numero = 10;
+while (numero >= 0) {
+    console.log(numero);
+    numero = numero - 1;
+}
+*/
+
+// Cuenta regresiva con un número ingresado por el usuario
+
+/*
+let numero = parseInt(prompt("Ingrese un número"));
+while (numero >= 0) {
+    console.log(numero);
+    numero = numero - 1;
+}
+*/
+
+// Cuenta progresiva con un número ingresado por el usuario
+
+let numeroMAX = parseInt(prompt("Ingrese un número"));
+let contador = 0 
+while (numeroMAX >= contador) {
+    console.log(contador);
+    contador = contador + 1;
+}
