066b7de7c41a02dc818e5fbfe3e9c2580956b767
ErikGzz03 <168046613+ErikGzz03@users.noreply.github.com>
2025-02-01 12:29:52 -0600

    Add files via upload

---
+++ Logia de progrmacion_funciones y listas/desafio_3.js
@@ -0,0 +1,47 @@
+// IMC 
+/*
+function IMC(peso, altura) {
+    return peso / (altura * altura);
+}
+
+console.log(IMC(65, 1.75));
+*/
+
+// Factorial
+/*
+function factorial(numero) {
+    if (numero === 0) return 1;
+    return numero * factorial(numero - 1);
+}
+console.log(factorial(5));
+*/
+
+// Conversión Reales a Dolares
+/*
+function RealesDolares(dinero) {
+    return dinero / 4.80;
+}
+console.log(RealesDolares(1000));
+*/
+
+/*
+function areaYPermimetroDeUnCirculo(radio) {
+    let area = 3.14 * radio * radio;
+    let permimetro = 2 * 3.14 * radio; 
+    return `El área es ${area} y el permimetro es ${permimetro}`;
+}
+
+console.log(areaYPermimetroDeUnCirculo(5));
+*/
+
+/*
+function tablaMultiplaciones (numero) {
+    let tabla = '';
+    for (let i = 1; i <= 10; i++) {
+        tabla += `${numero} x ${i} = ${numero * i}\n`;
+    }
+    return tabla;
+}
+
+console.log(tablaMultiplaciones(5));
+*/
